ef3801a689e3a5c59f109d46634f6c55e2eda8e2
nateL15 <nathanlass@gmail.com>
2017-11-30 18:51:52 -0500

    Roll back

---
+++ db/migrate/20171129170847_create_notes.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateNotes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :notes do |t|
+      t.string :text
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
